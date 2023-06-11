74f776d72147e2c6499dd2b512c992086f285b46
Denys Pugachov <pugachovdenys@gmail.com>
2023-06-11 13:10:14 +0200

    add path form current dir

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require("express")
+const path = require("path")
 
-// create an express server
-const app = express()
+const app = express()// create an express server
 const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, "../public") // __dirname = current directory (make path from current directory)
 
 
 // Serving static files
-app.use(express.static(`public`))
+app.use(express.static(publicDirectoryPath))
 
 // start express server 
 app.listen(port, () => {

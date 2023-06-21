0ef13b5438f2dd66991905ba7038bd90e26bf0fa
Denys Pugachov <pugachovdenys@gmail.com>
2023-06-21 20:52:59 +0200

    154.1: create basic server with public folder

--- src/index.js
+++ src/index.js
@@ -1,25 +1,15 @@
-const express = require("express")
-const app = express()
-const http = require("http")
 const path = require("path")
-const server = http.createServer(app)// create basic server
-const { Server } = require("socket.io")
-const io = new Server(server) //initialize a new instance of socket.io by passing the server (the HTTP server) object. 
+const express = require("express")
+const http = require("http")
+
+const app = express()
+const server = http.createServer(app)
 
 const port = process.env.PORT || 3000
-const publicDirectoryPath = path.join(__dirname, '../public')
+const publicDirPath = path.join(__dirname, "../public")
 
-app.use(express.static(publicDirectoryPath))// use public to serve files
+app.use(express.static(publicDirPath))
 
-
-io.on("connection", socket => {
-    socket.on("chat message", msg => {
-        io.emit("chat message", msg)
-        // console.log(`message: ${msg}`)
-    })
-})
-
-
-server.listen(port, () => {
-    console.log(`Chat-app listening on port ${port}...`);
+app.listen(port, () => {
+    console.log(`Server is listen on port:${port}...`)
 })

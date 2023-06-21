c7882823973886196accaeafa41fd4f6d59e3d83
Denys Pugachov <pugachovdenys@gmail.com>
2023-06-21 20:14:33 +0200

    socket chat example from public, seprate js,css

--- src/index.js
+++ src/index.js
@@ -1,29 +1,25 @@
 const express = require("express")
 const app = express()
 const http = require("http")
-const server = http.createServer(app)
+const path = require("path")
+const server = http.createServer(app)// create basic server
 const { Server } = require("socket.io")
 const io = new Server(server) //initialize a new instance of socket.io by passing the server (the HTTP server) object. 
 
 const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.use(express.static(publicDirectoryPath))// use public to serve files
 
-app.get(`/`, (req, res) => {
-    res.sendFile(__dirname + `/index.html`)
-})
 
 io.on("connection", socket => {
-    // console.log("user connected.")
     socket.on("chat message", msg => {
         io.emit("chat message", msg)
         // console.log(`message: ${msg}`)
     })
 })
 
-// start express server 
+
 server.listen(port, () => {
     console.log(`Chat-app listening on port ${port}...`);
 })
-
-// https://socket.io/get-started/chat
-

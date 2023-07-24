971c3b47d1167e104af93bae8aa5d6443542180d
Denys Pugachov <pugachovdenys@gmail.com>
2023-07-24 22:14:18 +0200

    169:add: getUser and getUsersInRoom

--- src/utils/users.js
+++ src/utils/users.js
@@ -36,29 +36,11 @@ const removeUser = id => {
     return users.splice(removedUserIndex, 1)[0]
 }
 
-
-
-// TESTS
-const user1 = {
-    id: 21,
-    username: "Den",
-    room: "  Home"
+const getUser = id => {
+    return users.find(user => user.id === id)
 }
 
-const user2 = {
-    id: 22,
-    username: "Den2",
-    room: "  Home"
+const getUsersInRoom = room => {
+    room = room.trim().toLowerCase()
+    return users.filter(user => user.room === room)
 }
-
-
-const testAddUser = addUser(user1)
-const testAddUser2 = addUser(user2)
-const testRemoveUser1 = removeUser(21)
-const testRemoveUser2 = removeUser(22)
-
-console.log('testAddUser :>> ', testAddUser);
-console.log('testAddUser2 :>> ', testAddUser2);
-console.log('testRemoveUser1 :>> ', testRemoveUser1);
-console.log('testRemoveUser2 :>> ', testRemoveUser2);
-console.log('users :>> ', users);
